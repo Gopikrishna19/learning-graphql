d62fdab09d928e703961b7937081c2428d95801c
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-18 17:52:57 -0600

    serve links from database

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import {connection} from './mysql';
 
 const app = express();
 
 app.use(express.static('dist'));
+
+app.get('/links', (req, res) => {
+  console.log('Serving: /links');
+
+  connection.query('SELECT * FROM links', (error, results, fields) => {
+    const headers = {'Content-Type': 'application/json'};
+
+    if (error) {
+      res.writeHead(500, headers);
+      res.end(JSON.stringify(error));
+    } else {
+      res.writeHead(200, headers);
+      res.end(JSON.stringify(results));
+    }
+  });
+});
 
 app.listen(8080);
