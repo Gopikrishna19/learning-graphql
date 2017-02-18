51988e5cee561236e45fd6e46e5e56fbf056ae42
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-18 16:10:49 -0600

    a simple react component

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = React.createClass({
+  render() {
+    return React.createElement('h3', null, 'Hello World!');
+  }
+});
+
+ReactDOM.render(React.createElement(HelloWorld), document.querySelector('.content'));
