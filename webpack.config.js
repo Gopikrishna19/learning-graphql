c05b4a3c929d2da7df2d72b4341bc081f1834bbe
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-18 16:00:20 -0600

    extract to html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/client/index.js',
   output: {
     filename: 'index.js',
     path: __dirname + '/dist'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './src/client/index.html'
+    })
+  ]
 };
