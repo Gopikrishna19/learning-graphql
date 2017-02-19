2817ae7f5c8627a416327a602697b2486e65d7c2
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-19 08:19:48 -0600

    write to distribution

--- src/server/schema/index.js
+++ src/server/schema/index.js
@@ -3,13 +3,14 @@ import Links from './links';
 import fs from 'fs';
 import {graphql} from 'graphql';
 import {introspectionQuery} from 'graphql/utilities';
+import {resolve} from 'path';
 
 export const setupGraphQL = async(app, connection) => {
 
   const schema = Links(connection);
   const SchemaJSON = await graphql(schema, introspectionQuery);
 
-  fs.writeFile('./schema.json', JSON.stringify(SchemaJSON, null, 2), err => { if (err) { throw err; } });
+  fs.writeFile(resolve('dist/schema.json'), JSON.stringify(SchemaJSON, null, 2), err => { if (err) { throw err; } });
 
   app.use('/links', GraphQLHttp({schema}));
 };
