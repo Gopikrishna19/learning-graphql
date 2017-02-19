d4a5871920f0e79cdec0a34f28558264d7b70374
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-18 19:32:20 -0600

    install babel-polyfill to use with async-await

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 import AppProvider from './provider';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'babel-polyfill';
 
 ReactDOM.render(<AppProvider/>, document.querySelector('.content'));
