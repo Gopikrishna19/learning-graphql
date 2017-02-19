1b4661b43dfbd00c929d116ef4f5b65bf22ff43f
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-18 20:04:39 -0600

    install isomorphic-fetch to use fetch API

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,6 @@ import AppProvider from './provider';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'babel-polyfill';
+import 'isomorphic-fetch';
 
 ReactDOM.render(<AppProvider/>, document.querySelector('.content'));
