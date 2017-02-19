675248b5fc10e57216b534b1a776d0fa2dda263e
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-18 22:11:20 -0600

    install and use body-parser for reading post data

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,14 @@
 import GraphQLHttp from 'express-graphql';
 import Links from './schema/links';
+import bodyParser from 'body-parser';
 import {connection} from './mysql';
 import express from 'express';
 
 const app = express();
 
 app.use(express.static('dist'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.listen(8080);
 
