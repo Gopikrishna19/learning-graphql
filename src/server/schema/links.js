c020ab4210b360f617bad4434ab5a697c71acfb1
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-19 11:40:13 -0600

    provide data as a store
    skip id as it is a link and relay requires id to be a string :(

--- src/server/schema/links.js
+++ src/server/schema/links.js
@@ -3,22 +3,21 @@ import {GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLList, GraphQLString
 export default connection => {
 
   const Link = new GraphQLObjectType({
-    name: 'Counter',
+    name: 'Link',
     fields: () => ({
-      id: {type: GraphQLInt},
       title: {type: GraphQLString},
       url: {type: GraphQLString}
     })
   });
 
   const Links = new GraphQLObjectType({
-    name: 'query',
+    name: 'Links',
     fields: () => ({
       links: {
         type: new GraphQLList(Link),
         resolve: () => new Promise((resolve, reject) => {
 
-          connection.query('SELECT * FROM links', (error, results) => {
+          connection.query('SELECT title, url FROM links', (error, results) => {
             if (error) {
               return reject(error);
             }
@@ -30,6 +29,16 @@ export default connection => {
     })
   });
 
-  return new GraphQLSchema({query: Links});
+  const Store = new GraphQLObjectType({
+    name: 'Store',
+    fields: () => ({
+      store: {
+        type: Links,
+        resolve: () => ({})
+      }
+    })
+  });
+
+  return new GraphQLSchema({query: Store});
 
 }
