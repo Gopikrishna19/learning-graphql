1b943dab54d3ad6b535a84cc48118eb3aa83f827
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-21 19:31:54 -0600

    mutation object to create a new link

--- src/server/schema/link.js
+++ src/server/schema/link.js
@@ -1,5 +1,5 @@
 import {GraphQLObjectType, GraphQLID, GraphQLNonNull, GraphQLString} from 'graphql';
-import {connectionDefinitions} from 'graphql-relay';
+import {connectionDefinitions, mutationWithClientMutationId} from 'graphql-relay';
 
 const Link = new GraphQLObjectType({
   name: 'Link',
@@ -13,7 +13,46 @@ const Link = new GraphQLObjectType({
   })
 });
 
-export default connectionDefinitions({
+const LinkConnection = connectionDefinitions({
   name: 'Link',
   nodeType: Link
 });
+
+export const CreateLink = (connection, Store) => mutationWithClientMutationId({
+  name: 'CreateLink',
+  inputFields: {
+    title: {type: new GraphQLNonNull(GraphQLString)},
+    url: {type: new GraphQLNonNull(GraphQLString)}
+  },
+  outputFields: {
+    link: {
+      type: LinkConnection.edgeType,
+      resolve: node => ({
+        cursor: node.id,
+        node
+      })
+    },
+    store: {
+      type: Store
+    }
+  },
+  mutateAndGetPayload: ({title, url}) => new Promise(
+    (resolve, reject) => connection.query(
+      'INSERT INTO links (title, url) VALUES (?, ?)',
+      [title, url],
+      (error, results) => {
+        if (error) {
+          return reject(error);
+        }
+
+        resolve({
+          id: results.insertId,
+          title,
+          url
+        });
+      }
+    )
+  )
+});
+
+export default LinkConnection;
